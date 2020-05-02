5c821afa55307ab120ebc4be86276f9b9123adda
Ying-Li Niu <64801511@qq.com>
2020-05-02 11:29:47 +0800

    update

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,84 +6,61 @@ using Newton;
 
 public class BallController : MonoBehaviour
 {
-    private float dt = 0.001f;
-    private Vector3 Pos;
-    private Vector3 Vel;
-    private Vector3[] Posarray;
-    private Vector3[] Velarray;
-    private int nball=4;
+    private int nball;
+    private float dt;
+    private Vector3[] X;
+    private Vector3[] V;
     private List<GameObject> balls;
-    //private var posStore = new List<Vector3>();
-    //--------------------------------------------------------------------------
-    private Vector6 Concatenate(Vector3 Pos, Vector3 Vel)
-    {
-        Vector6 XV;
-        XV.x = Pos.x;
-        XV.y = Pos.y;
-        XV.z = Pos.z;
-        XV.vx = Vel.x;
-        XV.vy = Vel.y;
-        XV.vz = Vel.z;
-        return XV;
-    }
-    //--------------------------------------------------------------------------
-    Vector6 NewXV(Vector3 X, Vector3 V, float dt)
-    {
-        Vector3 a = new Vector3(0.0f, -10.0f, 0.0f);
-        //----------------------------------------------------------------------
-        V.x = V.x + a.x * dt / 2.0f;
-        V.y = V.y + a.y * dt / 2.0f;
-        V.z = V.z + a.z * dt / 2.0f;
-        //----------------------------------------------------------------------
-        X.x = X.x + V.x * dt;
-        X.y = X.y + V.y * dt;
-        X.z = X.z + V.z * dt;
-        //----------------------------------------------------------------------
-        V.x = V.x + a.x * dt / 2.0f;
-        V.y = V.y + a.y * dt / 2.0f;
-        V.z = V.z + a.z * dt / 2.0f;
-        //----------------------------------------------------------------------
-        if (X.y <= -0.0f)
-        {
-            V.y = -V.y * 0.95f;
-        }
-        return Concatenate(X, V);
-    }
-    Vector3 getPos(Vector6 XV)
-    {
-        Vector3 X = new Vector3(XV.x, XV.y, XV.z);
-        return X;
-    }
-    Vector3 getVel(Vector6 XV)
-    {
-        Vector3 V = new Vector3(XV.vx, XV.vy, XV.vz);
-        return V;
-    }
+    private List<MoveController> ballscripts;
     public GameObject Ballpref;
-        
+    //--------------------------------------------------------------------------
+    void update_xv(int N, Vector3[] X, Vector3[] V, float dt)
+    {
+        // C# 函数2 (传值与传址)
+        // https://www.cnblogs.com/mdnx/archive/2012/09/04/2671060.html
+        for (int i=0; i<N; i++)
+        { 
+            Vector3 a = new Vector3(0.0f, -10.0f, 0.0f);
+            //------------------------------------------------------------------
+            V[i].x = V[i].x + a.x * dt / 2.0f;
+            V[i].y = V[i].y + a.y * dt / 2.0f;
+            V[i].z = V[i].z + a.z * dt / 2.0f;
+            //------------------------------------------------------------------
+            X[i].x = X[i].x + V[i].x * dt;
+            X[i].y = X[i].y + V[i].y * dt;
+            X[i].z = X[i].z + V[i].z * dt;
+            //------------------------------------------------------------------
+            V[i].x = V[i].x + a.x * dt / 2.0f;
+            V[i].y = V[i].y + a.y * dt / 2.0f;
+            V[i].z = V[i].z + a.z * dt / 2.0f;
+            //------------------------------------------------------------------
+            if (X[i].y <= -0.0f)
+            {
+                V[i].y = -V[i].y * 0.98f;
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        dt = 0.05f;
-        balls    = new List<GameObject>();
+        dt        = 0.05f;
+        nball     = 4;
+        balls     = new List<GameObject>();
+        ballscripts = new List<MoveController>();
         
-        Posarray = new Vector3[nball];
-        Velarray = new Vector3[nball];
-        int j = 0;
+        X = new Vector3[nball];
+        V = new Vector3[nball];
         for (int i = 0; i < nball; i++)
         {
             GameObject newBall = Instantiate(Ballpref);
             MoveController ballscript = newBall.GetComponent<MoveController>();
             newBall.transform.parent = transform;
-            j = 10 +i * 5;
-            Pos= new Vector3(j, 20, 20);
-            Vel = new Vector3(0,0,0);
-            newBall.transform.position = Pos;
-            Posarray[i]=newBall.transform.position;
-            Velarray[i]= ballscript.v;
+            ballscript.x = new Vector3(10.0f + i * 5.0f, 20.0f, 20.0f);
+            ballscript.v = new Vector3(0.0f,0.0f,0.0f);
+            newBall.transform.position = ballscript.x;
             balls.Add(newBall.gameObject);
-            Debug.Log("pos--0--" + i + ballscript.v);
-
+            ballscripts.Add(ballscript);
+            Debug.Log("debug-0" + i + ballscripts[i].x);
         }
     }
 
@@ -92,19 +69,19 @@ public class BallController : MonoBehaviour
     {
         for (int i = 0; i < nball; i++)
         {
-            Pos = Posarray[i];
-            Vel = Velarray[i];
-            Debug.Log("pos--1--" + i +Posarray[i] + Velarray[i]);
-            Vector6 XV = NewXV(Pos, Vel, dt);
-            Pos = getPos(XV);
-            Vel = getVel(XV);
-            Posarray[i] = Pos;
-            Velarray[i] = Vel;
-            Debug.Log("pos--2--" + i + Posarray[i] + Velarray[i]);
+            Debug.Log("debug-1"+ i + ballscripts[i].x);
+            X[i] = ballscripts[i].x;
+            V[i] = ballscripts[i].v;
+        }
+        update_xv(nball, X, V, dt);
+        for (int i = 0; i < nball; i++)
+        {
+            ballscripts[i].x = X[i];
+            ballscripts[i].v = V[i];
         }
         for (int i = 0; i < nball; i++)
         {
-            balls[i].transform.position = Posarray[i];
+            balls[i].transform.position = ballscripts[i].x;
         }
     }
 }
